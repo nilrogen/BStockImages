d2f8c2479a04035dfee6e0ca316abc95916c510c
nilrogen <nilrogen@github.com>
2017-11-13 12:03:30 -0500

    Fixed config to fit work with linux

--- config.py
+++ config.py
@@ -3,12 +3,16 @@ import os
 import json
 
 def getDropboxPath():
+    # Gets the Dropbox path depending on the platform
     fin = None
-    if sys.platform == 'linux2':
-        fin = open('~/.dropbox/info.json')
-    elif sys.platform == 'win32':
+    print(sys.platform)
+    if sys.platform == 'linux':
+        fin = open(os.path.join(os.getenv('HOME'), '.dropbox/info.json'))
+    if sys.platform == 'win32':
         fin = open(os.path.join(os.getenv('LOCALAPPDATA'), 'Dropbox\info.json'))
-    
+    else: # sys.platform == 'linux':
+        fin = open(os.path.join(os.getenv('HOME'), '.dropbox/info.json'))
+        
     jsn = json.load(fin)
     fin.close()
 
@@ -18,9 +22,3 @@ _DROPBOX = getDropboxPath()
 _MANIFEST_PATH = os.path.join(_DROPBOX, 'Marketplace Images/Costco Manifest')
 _IMAGES_PATH = os.path.join(_DROPBOX, 'Marketplace Images/Costco Images')
 _FNAME = os.path.join(_MANIFEST_PATH, 'AllFiles.json')
-      
-
-#_SAVE_PATH = '/home/michael/Desktop/Output/'
-#_SAVE_PATH = '/cygdrive/c/Users/Mike Work/Desktop'
-#_SAVE_PATH = "C:\\Users\\Mike Work\\Desktop"
-
