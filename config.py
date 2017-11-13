431285fdce711676adcc624f7da6baeeea985027
U-LAPTOP\Mike Work <nilrogen@gmail.com>
2017-11-13 11:53:16 -0500

    Made config crossplatform

--- config.py
+++ config.py
@@ -1,19 +1,26 @@
-import os.path
+import sys
+import os
+import json
 
-#_MANIFEST_PATH = '/home/michael/Dropbox/Marketplace Images/Costco Manifest'
-#_IMAGES_PATH = '/home/michael/Dropbox/Marketplace Images/Costco Images'
-#_FNAME = '/home/michael/Desktop/AllFiles.csv'
+def getDropboxPath():
+    fin = None
+    if sys.platform == 'linux2':
+        fin = open('~/.dropbox/info.json')
+    elif sys.platform == 'win32':
+        fin = open(os.path.join(os.getenv('LOCALAPPDATA'), 'Dropbox\info.json'))
+    
+    jsn = json.load(fin)
+    fin.close()
 
-_MANIFEST_PATH = '/cygdrive/c/Users/Mike Work/Dropbox/Marketplace Images/Costco Manifest'
-_IMAGES_PATH = '/cygdrive/c/Users/Mike Work/Dropbox/Marketplace Images/Costco Images'
-#_FNAME = '/home/michael/Desktop/AllFiles.csv'
+    return jsn['personal']['path']
 
-
-#_MANIFEST_PATH = "C:\\Users\\Mike Work\\Dropbox\\Marketplace Images\\Costco Manifest"
-#_IMAGES_PATH = "C:\\Users\\Mike Work\\Dropbox\\Marketplace Images\\Costco Images"
+_DROPBOX = getDropboxPath()
+_MANIFEST_PATH = os.path.join(_DROPBOX, 'Marketplace Images/Costco Manifest')
+_IMAGES_PATH = os.path.join(_DROPBOX, 'Marketplace Images/Costco Images')
 _FNAME = os.path.join(_MANIFEST_PATH, 'AllFiles.json')
-
+      
 
 #_SAVE_PATH = '/home/michael/Desktop/Output/'
 #_SAVE_PATH = '/cygdrive/c/Users/Mike Work/Desktop'
 #_SAVE_PATH = "C:\\Users\\Mike Work\\Desktop"
+
